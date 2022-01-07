eab54eee3614317c261d1460e987e5fd7d2fc606
Agiftsany <agiftsanyazhar@gmail.com>
2022-01-08 00:08:33 +0700

    oke bismillah

--- config/config.php
+++ config/config.php
@@ -12,6 +12,6 @@
 
     // Tes koneksi
     if ( !$mysqli ){
-        echo "Koneksi database gagal: ".mysqli_error($mysqli);
+        echo "Koneksi database gagal! ".mysqli_error($mysqli);
     }
 ?>
